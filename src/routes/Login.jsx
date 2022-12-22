bdddca2972650cc7936171974bbeadf9c30b05e7
Wellington <wcastillo2@utmachala.edu.ec>
2022-12-21 23:02:07 -0500

    test02

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
-  return <form>Formulario WECM</form>;
+export const Login = () => {
+  const [usuario, setUsuario] = useState({
+    nombre: "",
+    password: "",
+    remember: true,
+  });
+
+  function handleChange(e) {
+    const aux = { ...usuario };
+    aux[e.target.name] = e.target.value;
+    setUsuario(aux);
+  }
+
+  return (
+    <div
+      className="container"
+      style={{ background: "lightgray", marginTop: 50, padding: 15 }}
+    >
+      <form>
+        <div>
+          <h1 style={{ color: "blue", textalign: "center" }}>
+            <strong>PRÁCTICA</strong>
+          </h1>
+          <h2 style={{ color: "aquamarine", textalign: "center" }}>
+            <strong>Iniciar Sesion</strong>
+          </h2>
+          <label htmlFor="txtusu">
+            <strong>Usuario</strong>
+          </label>
+          <input
+            type="text"
+            id="txtusu"
+            name="nombre"
+            style={{ textAlign: "center" }}
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="txtpas">
+            <strong>Contraseña</strong>
+          </label>
+          <input
+            type="password"
+            id="txtpas"
+            name="password"
+            style={{ textAlign: "center" }}
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="topping">
+          <input
+            type="checkbox"
+            id="topping"
+            name="remember"
+            onChange={handleChange}
+          />
+          Remember
+        </div>
+
+        <input
+          type="button"
+          className="btn btn-danger"
+          value="Login"
+          onClick={(e) => console.log(usuario)}
+        />
+      </form>
+    </div>
+  );
 };
-
-export default Login;
